b46e073dd1f9d79adb1093b1266d93d4289f019b
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2024-11-26 15:45:57 +0330

    Change rotation of the sensors and add line_following logic

--- line_follower_epuck/controllers/epuck_line_follower_controller/epuck_line_follower_controller.py
+++ line_follower_epuck/controllers/epuck_line_follower_controller/epuck_line_follower_controller.py
@@ -1,36 +1,46 @@
-"""epuck_line_follower_controller controller."""
-
 from controller import Robot
 
-def run_robot(robot: Robot):
-    time_step = 32
+def run_robot(robot):
+    time_step = int(robot.getBasicTimeStep())
     max_speed = 6.28
 
-    left_motor = robot.getMotor('left wheel motor')
-    right_motor = robot.getMotor('right wheel motor')
+    left_motor = robot.getDevice('left wheel motor')
+    right_motor = robot.getDevice('right wheel motor')
     left_motor.setPosition(float('inf'))
     right_motor.setPosition(float('inf'))
     left_motor.setVelocity(0.0)
     right_motor.setVelocity(0.0)
 
-    left_ir = robot.getDistanceSensor('ir0')
-    left_ir.enable(time_step)
-
-    right_ir = robot.getDistanceSensor('ir1')
-    right_ir.enable(time_step)
+    left_light_sensor = robot.getDevice('ir0')
+    right_light_sensor = robot.getDevice('ir1')
+    left_light_sensor.enable(time_step)
+    right_light_sensor.enable(time_step)
 
     while robot.step(time_step) != -1:
-        left_ir_value = left_ir.getValue()
-        right_ir_value = right_ir.getValue()
+        left_value = left_light_sensor.getValue()
+        right_value = right_light_sensor.getValue()
 
-        print(left_ir_value, right_ir_value)
+        print(f"Left Sensor: {left_value}, Right Sensor: {right_value}")
 
-        left_speed = max_speed
-        right_speed = max_speed
+        # Determine threshold based on sensor readings
+        threshold = (left_value + right_value) / 2
+
+        # Implement line-following logic
+        if left_value < threshold and right_value >= threshold:
+            # Turn left
+            left_speed = max_speed * 0.5
+            right_speed = max_speed
+        elif right_value < threshold and left_value >= threshold:
+            # Turn right
+            left_speed = max_speed
+            right_speed = max_speed * 0.5
+        else:
+            # Move forward
+            left_speed = max_speed
+            right_speed = max_speed
 
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
-
 
 if __name__ == '__main__':
     epuck = Robot()
