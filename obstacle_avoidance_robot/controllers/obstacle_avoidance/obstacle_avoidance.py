f6b5c50caea818381d60acb3fa75a8f29fa1c909
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2024-11-27 01:30:38 +0330

    Add obstacle detection using proximity sensors

--- obstacle_avoidance_robot/controllers/obstacle_avoidance/obstacle_avoidance.py
+++ obstacle_avoidance_robot/controllers/obstacle_avoidance/obstacle_avoidance.py
@@ -1,32 +1,80 @@
 """obstacle_avoidance controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
 
-# create the Robot instance.
-robot = Robot()
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+def enable_sensors(robot: Robot, timestep: int):
+    ps = []
+    ps_names = ['ps' + str(i) for i in range(8)]
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+    for name in ps_names:
+        sensor = robot.getDevice(name)
+        sensor.enable(timestep)
+        ps.append(sensor)
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+    return ps
 
-    # Process sensor data here.
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
+def enable_camera(robot: Robot, timestep: int):
+    camera = robot.getDevice('camera')
+    camera.enable(timestep)
+
+    return camera
+
+
+def detect_obstacles_by_sensors(ps_list, max_speed: float, distance_threshold: float=100.0):
+    ps_values = [sensor.getValue() for sensor in ps_list]
+    
+    print(ps_values)
+
+    right_obstacle = ps_values[0] > distance_threshold or ps_values[1] > distance_threshold or ps_values[2] > distance_threshold
+    left_obstacle = ps_values[5] > distance_threshold or ps_values[6] > distance_threshold or ps_values[7] > distance_threshold
+
+    if left_obstacle:
+        return max_speed, -max_speed
+
+    elif right_obstacle:
+        return -max_speed, max_speed
+
+    else:
+        return max_speed, max_speed
+
+
+
+def detect_obstacles_by_camera(camera_device):
     pass
 
-# Enter here exit cleanup code.
+
+def start_engine(robot: Robot, left_velocity: float, right_velocity: float):
+    left_motor = robot.getDevice('left wheel motor')
+    right_motor = robot.getDevice('right wheel motor')
+
+    left_motor.setPosition(float('inf'))
+    right_motor.setPosition(float('inf'))
+
+    left_motor.setVelocity(left_velocity)
+    right_motor.setVelocity(right_velocity)
+
+    return left_motor, right_motor
+
+
+
+if __name__ == '__main__':
+    epuck = Robot()
+
+    timestep = int(epuck.getBasicTimeStep())
+
+    max_speed = 6.28
+
+    left_motor, right_motor = start_engine(epuck, left_velocity=0.0, right_velocity=0.0)
+
+    ps_list = enable_sensors(epuck, timestep)
+    camera = enable_camera(epuck, timestep)
+
+    while epuck.step(timestep) != -1:
+
+        left_speed, right_speed = detect_obstacles_by_sensors(ps_list, max_speed)
+
+        left_motor.setVelocity(left_speed)
+        right_motor.setVelocity(right_speed)
+
