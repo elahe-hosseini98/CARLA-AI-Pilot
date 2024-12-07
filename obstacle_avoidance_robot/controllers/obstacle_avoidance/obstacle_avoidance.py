0c7e57002197d0cfe0e703513e19f5d700d6ca6f
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2024-12-07 21:29:08 +0330

    remove unused imported package

--- obstacle_avoidance_robot/controllers/obstacle_avoidance/obstacle_avoidance.py
+++ obstacle_avoidance_robot/controllers/obstacle_avoidance/obstacle_avoidance.py
@@ -22,13 +22,18 @@ def enable_camera(robot: Robot, timestep: int):
     return camera
 
 
-def detect_obstacles_by_sensors(ps_list, max_speed: float, distance_threshold: float=100.0):
+def detect_obstacles_by_sensors(ps_list, max_speed: float, distance_threshold: float=90.0):
     ps_values = [sensor.getValue() for sensor in ps_list]
     
     print(ps_values) # these get higher as the robot gets closer to an obstacle
 
-    right_obstacle = ps_values[0] > distance_threshold or ps_values[1] > distance_threshold or ps_values[2] > distance_threshold
-    left_obstacle = ps_values[5] > distance_threshold or ps_values[6] > distance_threshold or ps_values[7] > distance_threshold
+    right_obstacle = (ps_values[0] > distance_threshold
+                      or ps_values[1] > distance_threshold
+                      or ps_values[2] > distance_threshold)
+
+    left_obstacle = (ps_values[5] > distance_threshold
+                     or ps_values[6] > distance_threshold
+                     or ps_values[7] > distance_threshold)
 
     if left_obstacle:
         return max_speed, -max_speed
@@ -40,8 +45,35 @@ def detect_obstacles_by_sensors(ps_list, max_speed: float, distance_threshold: f
         return max_speed, max_speed
 
 
-def detect_obstacles_by_camera(camera_device):
-    pass
+def detect_obstacles_by_camera(camera_device, obstacle_threshold=80):
+    image = camera_device.getImageArray()
+
+    if image is None:
+        return False
+
+    height = camera_device.getHeight()
+    width = camera_device.getWidth()
+
+    grayscale = []
+    for y in range(height):
+        row = []
+        for x in range(width):
+            r = image[y][x][0]
+            g = image[y][x][1]
+            b = image[y][x][2]
+            gray = (r + g + b) / 3
+            row.append(gray)
+        grayscale.append(row)
+
+    grayscale = np.array(grayscale)
+
+    central_region = grayscale[height // 3: 2 * height // 3, width // 3: 2 * width // 3]
+    mean_intensity = np.mean(central_region)
+
+    if mean_intensity < obstacle_threshold:
+        return True
+    else:
+        return False
 
 
 def start_engine(robot: Robot):
@@ -55,7 +87,6 @@ def start_engine(robot: Robot):
     right_motor.setVelocity(0.0)
 
     return left_motor, right_motor
-
 
 
 if __name__ == '__main__':
@@ -72,7 +103,7 @@ if __name__ == '__main__':
 
     while epuck.step(timestep) != -1:
 
-        left_speed, right_speed = detect_obstacles_by_sensors(ps_list, max_speed)
+        left_speed, right_speed = detect_obstacles_by_sensors(ps_list, max_speed=max_speed)
 
         left_motor.setVelocity(left_speed)
         right_motor.setVelocity(right_speed)
