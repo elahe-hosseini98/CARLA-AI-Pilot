3fd8a16519a9c2c4e65b02c85415fa75c2e235b3
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2024-12-27 20:31:47 +0330

    add velocity controller method

---
+++ micro_mouse/controllers/micro_mouse_controller/velocity_adjustment.py
@@ -0,0 +1,48 @@
+from pathfindings.right_hand_path import navigate_with_ir
+
+
+def sharp_left_turn(robot, left_motor, right_motor, timestep, max_speed, turn_duration):
+    left_motor.setVelocity(-max_speed)
+    right_motor.setVelocity(max_speed)
+
+    start_time = robot.getTime()
+    while robot.getTime() - start_time < turn_duration:
+        robot.step(timestep)
+
+    left_motor.setVelocity(max_speed)
+    right_motor.setVelocity(max_speed)
+
+
+def sharp_right_turn(robot, left_motor, right_motor, timestep, max_speed, turn_duration):
+    left_motor.setVelocity(max_speed)
+    right_motor.setVelocity(-max_speed)
+
+    start_time = robot.getTime()
+    while robot.getTime() - start_time < turn_duration:
+        robot.step(timestep)
+
+    left_motor.setVelocity(max_speed)
+    right_motor.setVelocity(max_speed)
+
+
+def go_straight(left_motor, right_motor, max_speed):
+    left_motor.setVelocity(max_speed)
+    right_motor.setVelocity(max_speed)
+
+
+def update_velocity_based_on_path(robot, ir_sensor_values, timestep, max_speed):
+    turn_duration = 0.47
+
+    left_motor = robot.getDevice('left wheel motor')
+    right_motor = robot.getDevice('right wheel motor')
+
+    direction = navigate_with_ir(ir_sensor_values) # 0: straight, 1: right, -1: left
+
+    if direction == 0: # go straight
+        return go_straight(left_motor, right_motor, max_speed)
+
+    elif direction == 1: # turn right
+        return sharp_right_turn(robot, left_motor, right_motor, timestep, max_speed, turn_duration)
+
+    else: # turn left
+        return sharp_left_turn(robot, left_motor, right_motor, timestep, max_speed, turn_duration)
