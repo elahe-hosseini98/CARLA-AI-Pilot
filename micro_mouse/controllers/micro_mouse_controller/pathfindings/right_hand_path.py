7f42dcb14c84c8e8b8256ab36e7c0dba209eccd9
Elaheh Hosseni <elahe.s.hs98@gmail.com>
2024-12-30 08:25:02 +0330

    Update navigation approach

--- micro_mouse/controllers/micro_mouse_controller/pathfindings/right_hand_path.py
+++ micro_mouse/controllers/micro_mouse_controller/pathfindings/right_hand_path.py
@@ -15,13 +15,21 @@ def navigate_with_ir(ir_sensor_values, front_distance_threshold=10):
     elif front_side < front_distance_threshold: return -1
 
 
-def navigate_with_custom_dist_sensor(dist_sensors, front_distance_threshold=0.1):
+def navigate_with_custom_dist_sensor(dist_sensors, front_distance_threshold=0.1, turning_distance_threshold=0.6):
     front_side = dist_sensors[0]
     right_side = dist_sensors[1]
     rear_side = dist_sensors[2]
     left_side = dist_sensors[3]
 
-    if right_side - left_side > 0.7: return 1
-    elif front_side < front_distance_threshold: return -1
-    elif right_side - left_side < 0.7: return 0
+    if ((front_side < 0.1 and right_side < 0.2) or
+        (left_side > 0.2 and front_side > 0.25 and rear_side > 0.2)):
+        return -1
 
+    elif right_side < 0.2 and left_side < 0.2: return 0
+
+    elif ((right_side > 0.7 or (right_side > 0.2 and left_side > 0.2) or
+          (right_side > 0.2 and front_side > 0.25 and rear_side > 0.2)) or
+          (front_side < 0.1 and right_side > 0.2)):
+        return 1
+
+    else: return 0
--- micro_mouse/controllers/micro_mouse_controller/velocity_adjustment.py
+++ micro_mouse/controllers/micro_mouse_controller/velocity_adjustment.py
@@ -1,8 +1,17 @@
 from pathfindings.right_hand_path import navigate_with_ir, navigate_with_custom_dist_sensor
 
 
+def keep_go_straight(robot, left_motor, right_motor, timestep, max_speed, turn_duration=0.4):
+    start_time = robot.getTime()
+    while robot.getTime() - start_time < turn_duration:
+        left_motor.setVelocity(max_speed)
+        right_motor.setVelocity(max_speed)
+        robot.step(timestep)
+
+
 def sharp_left_turn(robot, left_motor, right_motor, timestep, max_speed, turn_duration):
-    print('turn left')
+    print('Sharp turning to left')
+
     left_motor.setVelocity(-max_speed)
     right_motor.setVelocity(max_speed)
 
@@ -10,12 +19,14 @@ def sharp_left_turn(robot, left_motor, right_motor, timestep, max_speed, turn_du
     while robot.getTime() - start_time < turn_duration:
         robot.step(timestep)
 
+    keep_go_straight(robot, left_motor, right_motor, timestep, max_speed)
+
     left_motor.setVelocity(max_speed)
-    right_motor.setVelocity(max_speed)
 
 
 def sharp_right_turn(robot, left_motor, right_motor, timestep, max_speed, turn_duration):
-    print('turn right')
+    print('Sharp turning to right')
+
     left_motor.setVelocity(max_speed)
     right_motor.setVelocity(-max_speed)
 
@@ -23,18 +34,35 @@ def sharp_right_turn(robot, left_motor, right_motor, timestep, max_speed, turn_d
     while robot.getTime() - start_time < turn_duration:
         robot.step(timestep)
 
-    left_motor.setVelocity(max_speed)
+    keep_go_straight(robot, left_motor, right_motor, timestep, max_speed)
+
     right_motor.setVelocity(max_speed)
 
 
-def go_straight(left_motor, right_motor, max_speed):
+def smooth_left_turn(left_motor, right_motor, max_speed):
+    print('Smooth turning to left')
+
+    left_motor.setVelocity(-max_speed)
+    right_motor.setVelocity(max_speed)
+
+
+def smooth_right_turn(left_motor, right_motor, max_speed):
+    print('Smooth turning to right')
+
+    left_motor.setVelocity(max_speed)
+    right_motor.setVelocity(-max_speed)
+
+
+def go_straight(robot, left_motor, right_motor, timestep, max_speed):
     print('go straight')
     left_motor.setVelocity(max_speed)
     right_motor.setVelocity(max_speed)
 
+    keep_go_straight(robot, left_motor, right_motor, timestep, max_speed, turn_duration=0.2)
+
 
 def update_velocity_based_on_path(robot, ir_sensor_values, timestep, max_speed):
-    turn_duration = 1.632
+    turn_duration = 1.63
 
     left_motor = robot.getDevice('left wheel motor')
     right_motor = robot.getDevice('right wheel motor')
@@ -42,7 +70,7 @@ def update_velocity_based_on_path(robot, ir_sensor_values, timestep, max_speed):
     direction = navigate_with_custom_dist_sensor(ir_sensor_values) # 0: straight, 1: right, -1: left
 
     if direction == 0: # go straight
-        return go_straight(left_motor, right_motor, max_speed)
+        return go_straight(robot, left_motor, right_motor, timestep, max_speed)
 
     elif direction == 1: # turn right
         return sharp_right_turn(robot, left_motor, right_motor, timestep, max_speed, turn_duration)
